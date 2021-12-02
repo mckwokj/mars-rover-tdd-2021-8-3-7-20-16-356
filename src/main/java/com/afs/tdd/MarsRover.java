7c0ee8863fa98d53a51435744b14cd8627395a3b
Johnson Kwok <kwokjo2@oocl.com>
2021-12-02 16:17:51 +0800

    [MarsRover] [Johnson Kwok] feat: craete test case 1

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,50 @@
+package com.afs.tdd;
+
+public class MarsRover {
+    private int locationX;
+    private int locationY;
+    private String direction;
+
+    public MarsRover(int locationX, int locationY, String direction) {
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.direction = direction;
+    }
+
+    public int getLocationX() {
+        return locationX;
+    }
+
+    public void setLocationX(int locationX) {
+        this.locationX = locationX;
+    }
+
+    public int getLocationY() {
+        return locationY;
+    }
+
+    public void setLocationY(int locationY) {
+        this.locationY = locationY;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public void executeCommand(String command) {
+        move();
+        getStatus();
+    }
+
+    public void move() {
+        this.locationY++;
+    }
+
+    public String getStatus() {
+        return null;
+    }
+}
---
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -0,0 +1,62 @@
+package com.afs.tdd;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MarsRoverTest {
+    @Test
+    void should_locationY_add_one_space_when_execute_command_given_direction_N_and_command_M () {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        String command = "M";
+
+        // when
+        marsRover.executeCommand(command);
+        String result = marsRover.getStatus();
+
+        // then
+        assertEquals("0 1 N", result);
+    }
+
+//    @Test
+//    void should_locationY_add_one_space_when_execute_command_given_direction_S_and_command_M () {
+//        // given
+//        MarsRover marsRover = new MarsRover(0, 0, "S");
+//        String command = "M";
+//
+//        // when
+//        marsRover.executeCommand(command);
+//        String result = marsRover.getStatus();
+//
+//        // then
+//        assertEquals("0 -1 S", result);
+//    }
+//
+//    @Test
+//    void should_locationY_add_one_space_when_execute_command_given_direction_E_and_command_M () {
+//        // given
+//        MarsRover marsRover = new MarsRover(0, 0, "E");
+//        String command = "M";
+//
+//        // when
+//        marsRover.executeCommand(command);
+//        String result = marsRover.getStatus();
+//
+//        // then
+//        assertEquals("1 0 E", result);
+//    }
+//
+//    @Test
+//    void should_locationY_add_one_space_when_execute_command_given_direction_W_and_command_M () {
+//        // given
+//        MarsRover marsRover = new MarsRover(0, 0, "W");
+//        String command = "M";
+//
+//        // when
+//        marsRover.executeCommand(command);
+//        String result = marsRover.getStatus();
+//
+//        // then
+//        assertEquals("-1 0 W", result);
+//    }
+}
